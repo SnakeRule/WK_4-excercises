35f3d6ebc5d62c6b3adc92d32f479bbaf361e4d1
Jere Valtanen <jerevaltanen@msn.com>
2016-01-20 14:44:52 +0200

    Updated

---
+++ Excercise 1/Elevator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise_1
+{
+    class Elevator
+    {
+        // Member field
+        private int floor = 1;
+
+        // Properties
+        public int Floor
+        {
+            get
+            {
+                return floor;
+            }
+            set
+            {
+                if (value > 0 && value < 6)
+                    floor = value;
+                else if (value < 1)
+                {
+                    Console.WriteLine("That floor is too low. Try again");
+                }
+                else Console.WriteLine("That floor is too high. Try again");
+            }
+        }
+    }
+}
---
+++ Excercise 1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool loop = true;
+            Elevator elevator = new Elevator();
+
+            Console.WriteLine("Welcome to the elevator");
+            do
+            {
+                Console.WriteLine("Elevator is now in floor {0}", elevator.Floor);
+                Console.WriteLine("Choose a floor 1-5");
+                elevator.Floor = int.Parse(Console.ReadLine());
+            } while (loop == true);
+        }
+    }
+}
