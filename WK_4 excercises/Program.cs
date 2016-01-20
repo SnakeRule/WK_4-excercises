a9dd65f892d8435b661186b071c1fd11ed4d1efb
Jere Valtanen <jerevaltanen@msn.com>
2016-01-20 12:36:48 +0200

    Created

---
+++ WK_4 excercises/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WK_4_excercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
