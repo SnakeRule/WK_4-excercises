6d440cef5d65d994fb6a6f0abfd1ebea65528aa2
SnakeRule <jerevaltanen@gmail.com>
2016-01-22 14:34:07 +0200

    Complete

---
+++ Excercise 5/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Radio radio = new Radio();
+            bool loop = true;
+            int input;
+            do
+            {
+                Console.WriteLine();
+                if (radio.power_on == true)
+                    Console.WriteLine("The radio is on");
+                else
+                    Console.WriteLine("The radio is off");
+                Console.WriteLine("Radio volume is {0} and the frequency is {1}", radio.Volume, radio.Frequency);
+                Console.WriteLine("1. Power On/Off");
+                Console.WriteLine("2. Change volume");
+                Console.WriteLine("3. Change frequency 2000,0-26000,0");
+                Console.WriteLine("4. Quit");
+                input = int.Parse(Console.ReadLine());
+
+                switch (input)
+                {
+                    case 1:
+                        if (radio.power_on == false)
+                            radio.power_on = true;
+                        else
+                            radio.power_on = false;
+                        break;
+                    case 2:
+                        {
+                            Console.Write("Set volume 0-9");
+                            radio.Volume = int.Parse(Console.ReadLine());
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.Write("Set frequency 2000,0-26000,0");
+                            radio.Frequency = double.Parse(Console.ReadLine());
+                            break;
+                        }
+                    case 4:
+                        loop = false;
+                        break;
+                }
+            } while (loop == true);
+        }
+    }
+}
---
+++ Excercise 5/Radio.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise_5
+{
+    class Radio
+    {
+        private int volume = 0;
+        private double frequency = 2000.0;
+
+        public int Volume
+        {
+            get
+            {
+                return volume;
+            }
+            set
+            {
+                if (power_on == true)
+                {
+                    if (value >= 0 && value <= 9)
+                        volume = value;
+                    else if (value < 0)
+                        Console.WriteLine("You're trying to set the volume below 0!");
+                    else
+                        Console.WriteLine("You're trying to set the volume above 9!");
+                }
+                else
+                    Console.WriteLine("The radio is not on! Can't set frequency");
+            }
+        }
+        public double Frequency
+        {
+            get
+            {
+                return frequency;
+            }
+            set
+            {
+                if (power_on == true)
+                {
+                    if (value >= 2000.0 && value <= 26000.0)
+                    frequency = value;
+                else if (value < 2000.0)
+                    Console.WriteLine("You're trying to set the frequency below 2000.0!");
+                else
+                    Console.WriteLine("You're trying to set the frequency above 26000.0!");
+                }
+                else
+                    Console.WriteLine("The radio is not on! Can't set frequency");
+            }
+        }
+        public bool power_on { get; set; }
+    }
+}
